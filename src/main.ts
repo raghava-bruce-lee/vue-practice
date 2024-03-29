c908a4a5fc0e3f195dd1647aae79d24c841daa66
Raghavendra <raghav@Raghavendras-MacBook-Air.local>
2024-03-29 10:50:46 +0530

    adding vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
