f17bc9c4ad3a5ad145ac1798b99f6b47407efeb0
raghava-bruce-lee <raghavendra1319@gmail.com>
2024-03-30 21:55:29 +0530

    add vuetify mdi icons

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,25 @@
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 export default createVuetify({
   components,
-  directives
+  directives,
+  icons: {
+    defaultSet: 'mdi' // This is already the default value - only for display purposes
+  },
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#0D47A1',
+          secondary: '#CFD8DC'
+        }
+      }
+    }
+  }
 })
