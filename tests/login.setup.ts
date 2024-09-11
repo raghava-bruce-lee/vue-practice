4218c9a4e17ad2f03835363c925e14150fb26c99
raghava-bruce-lee <raghavendra1319@gmail.com>
2024-09-11 19:35:54 +0530

    just for testing

--- tests/login.setup.ts
+++ tests/login.setup.ts
@@ -1,19 +1,19 @@
-import { test as setup } from '@playwright/test';
-import { fileURLToPath } from 'url';
-import path from 'path';
+// import { test as setup } from '@playwright/test';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
 
-const authFile = path.join(
-  path.dirname(fileURLToPath(import.meta.url)),
-  '../playwright/.auth/user.json'
-);
+// const authFile = path.join(
+//   path.dirname(fileURLToPath(import.meta.url)),
+//   '../playwright/.auth/user.json'
+// );
 
-setup('authenticate', async ({ page }) => {
-  await page.goto('/', { waitUntil: 'networkidle' });
+// setup('authenticate', async ({ page }) => {
+//   await page.goto('/', { waitUntil: 'networkidle' });
 
-  await page.fill('#input-0', process.env.USER_NAME as string);
-  await page.fill('#input-2', process.env.PASSWORD as string);
-  await page.getByRole('button', { name: 'LOGIN' }).click();
+//   await page.fill('#input-0', process.env.USER_NAME as string);
+//   await page.fill('#input-2', process.env.PASSWORD as string);
+//   await page.getByRole('button', { name: 'LOGIN' }).click();
 
-  await page.waitForURL(process.env.APP_URL as string);
-  await page.context().storageState({ path: authFile });
-});
+//   await page.waitForURL(process.env.APP_URL as string);
+//   await page.context().storageState({ path: authFile });
+// });
